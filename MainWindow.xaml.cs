97649af7b61a816bd08229368549d1be91e13800
stust <stust@Lib11F-20>
2023-12-28 12:31:55 +0800

    加入專案檔案。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.Net.Http;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=JSON";
+        AQIData aqidata = new AQIData();
+        List<Field> fields = new List<Field>();
+        List<Record> records = new List<Record>();
+        List<Record> selectedRecords = new List<Record>();
+        SeriesCollection seriesCollection = new SeriesCollection();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UrlTextBox.Text = defaultURL;
+
+        }
+
+        private async void FetchDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBox.Text = "抓取網路中...";
+            string jsonData = await FetchWebDataAsync(defaultURL);
+            ContentTextBox.Text = jsonData;
+
+            aqidata = JsonSerializer.Deserialize<AQIData>(jsonData);
+
+            fields = aqidata.fields.ToList();
+            records = aqidata.records.ToList();
+            selectedRecords = records;
+            StatusTextBlock.Text = $"共有 {fields.Count}個欄位， {records.Count} 筆記錄";
+
+            DisplayAQIData();
+        }
+
+        private void DisplayAQIData()
+        {
+            RecordDataGrid.ItemsSource = records;
+
+            Record record = records[0];
+            DataWrapPanel.Children.Clear();
+
+            foreach (Field field in fields)
+            {
+                var propertyInfo = record.GetType().GetProperty(field.id);
+                if (propertyInfo != null)
+                {
+                    var value = propertyInfo.GetValue(record) as string;
+                    if (double.TryParse(value, out double v))
+                    {
+                        CheckBox cb = new CheckBox
+                        {
+                            Content = field.info.label,
+                            Tag = field.id,
+                            Margin = new Thickness(3),
+                            FontSize = 14,
+                            FontWeight = FontWeights.Bold,
+                            Width = 120
+                        };
+                        cb.Checked += UpdateChart;
+                        cb.Unchecked += UpdateChart;
+                        DataWrapPanel.Children.Add(cb);
+                    }
+                }
+            }
+        }
+
+        private void UpdateChart(object sender, RoutedEventArgs e)
+        {
+            seriesCollection.Clear();
+
+            foreach (CheckBox cb in DataWrapPanel.Children)
+            {
+                if (cb.IsChecked == true)
+                {
+                    List<String> labels = new List<String>();
+                    String? tag = cb.Tag.ToString();
+                    ColumnSeries columnSeries = new ColumnSeries();
+                    ChartValues<double> values = new ChartValues<double>();
+
+                    foreach (Record record in selectedRecords)
+                    {
+                        var propertyInfo = record.GetType().GetProperty(tag);
+                        if (propertyInfo != null)
+                        {
+                            String? value = propertyInfo.GetValue(record) as String;
+                            if (double.TryParse(value, out double v))
+                            {
+                                values.Add(v);
+                                labels.Add(record.sitename);
+                            }
+                        }
+                    }
+                    columnSeries.Values = values;
+                    columnSeries.Title = tag;
+                    columnSeries.LabelPoint = point => $"{labels[(int)point.X]}: {point.Y.ToString()}";
+                    seriesCollection.Add(columnSeries);
+                }
+            }
+            AQIChart.Series = seriesCollection;
+        }
+
+        private async Task<string> FetchWebDataAsync(string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    return await client.GetStringAsync(url);
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"錯誤：{ex.Message}";
+            }
+        }
+
+        private void RecordDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+
+        private void RecordDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedRecords = RecordDataGrid.SelectedItems.Cast<Record>().ToList();
+            StatusTextBlock.Text = $"共選取 {selectedRecords.Count} 筆記錄";
+        }
+    }
+}
